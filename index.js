9659283d9117af96749a0f67f865c61df3d614de
fukuchiyuki1225 <rkrn.zerozerozerooox@gmail.com>
2021-12-07 19:02:12 +0900

    座標変化を記録するよう変更

--- index.js
+++ index.js
@@ -3,39 +3,62 @@
 const canvas = $('#canvas');
 const canvasDom = canvas[0];
 const ctx = canvasDom.getContext('2d');
-let drawingFlag = false;
-let startX = 0;
-let startY = 0;
 const offsetX = canvasDom.getBoundingClientRect().left;
 const offsetY = canvasDom.getBoundingClientRect().top;
+let drawFlag = false;
+let startX = 0;
+let startY = 0;
+let x = 0;
+let y = 0;
+let recordFlag = false;
+let recordTimer;
 
 canvasDom.width = 482;
 canvasDom.height = 362;
-
 ctx.lineCap = 'round';
 ctx.strokeStyle = 'lightskyblue';
 ctx.lineWidth = 20;
 
 canvas.mousemove(function (e) {
-  if (!drawingFlag) return;
-  const x = e.clientX - offsetX;
-  const y = e.clientY - offsetY;
-  ctx.beginPath();
-  ctx.moveTo(startX, startY);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-  ctx.closePath();
-  startX = x;
-  startY = y;
+  if (!drawFlag) return;
+  x = e.clientX - offsetX;
+  y = e.clientY - offsetY;
+  drawPath(x, y);
+  if (!recordFlag) {
+    recordTimer = setInterval(recordCoordinate, 200);
+    recordFlag = true;
+  }
 });
 
 canvas.mousedown(function (e) {
   ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
-  drawingFlag = true;
+  drawFlag = true;
   startX = e.clientX - offsetX;
   startY = e.clientY - offsetY;
 });
 
 canvas.mouseup(function () {
-  drawingFlag = false;
+  drawFlag = false;
+  recordFlag = false;
+  clearInterval(recordTimer);
+  console.log(inputX);
 });
+
+function drawPath(endX, endY) {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+  ctx.closePath();
+  startX = endX;
+  startY = endY;
+}
+
+let inputX = [];
+let inputY = [];
+
+let recordCoordinate = function () {
+  console.log('record');
+  inputX.push(x);
+  inputY.push(y);
+};
