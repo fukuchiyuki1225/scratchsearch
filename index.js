58d1937ac6e098f9e61f091d6eb4b37bed35d0f7
fukuchiyuki1225 <rkrn.zerozerozerooox@gmail.com>
2021-12-07 17:46:55 +0900

    線（軌跡）を書けるよう変更

--- index.js
+++ index.js
@@ -4,22 +4,38 @@ const canvas = $('#canvas');
 const canvasDom = canvas[0];
 const ctx = canvasDom.getContext('2d');
 let drawingFlag = false;
+let startX = 0;
+let startY = 0;
+const offsetX = canvasDom.getBoundingClientRect().left;
+const offsetY = canvasDom.getBoundingClientRect().top;
 
 canvasDom.width = 482;
 canvasDom.height = 362;
 
 ctx.lineCap = 'round';
-ctx.strokeDtyle = 'lightskyblue';
-ctx.lineWidth = '20px';
+ctx.strokeStyle = 'lightskyblue';
+ctx.lineWidth = 20;
 
-canvas.mousemove(function () {
+canvas.mousemove(function (e) {
   console.log('drawing');
   if (!drawingFlag) return;
+  const x = e.clientX - offsetX;
+  const y = e.clientY - offsetY;
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+  ctx.closePath();
+  startX = x;
+  startY = y;
 });
 
-canvas.mousedown(function () {
+canvas.mousedown(function (e) {
   console.log('mousedown');
+  ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
   drawingFlag = true;
+  startX = e.clientX - offsetX;
+  startY = e.clientY - offsetY;
 });
 
 canvas.mouseup(function () {
